4703cb060a833820bc352ae1f56f2d5f6e9cb488
Jonas Strømsodd <jonas.stromsodd@gmail.com>
2016-02-28 21:36:36 +0100

    Add endpoint to fetch list of programs. Change detail endpoint to use id instead of name

--- server/app/model/Program.js
+++ server/app/model/Program.js
@@ -4,14 +4,5 @@ const programSchema = new mongoose.Schema({
   name: String
 });
 
-programSchema.statics.findByName = function(name) {
-  return this.findOne({
-    name: new RegExp(name, 'i')
-  });
-};
-
-
-
-
 const Program = mongoose.model('Program', programSchema);
 export default Program;
