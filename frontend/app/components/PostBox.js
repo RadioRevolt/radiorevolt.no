8447c6d0320e98c961f7b2a0367527f2ac8edc7c
Trygve Bertelsen Wiig <trygvebw@gmail.com>
2016-04-11 00:55:17 +0200

    Image formatting

--- frontend/app/components/PostBox.js
+++ frontend/app/components/PostBox.js
@@ -15,8 +15,8 @@ var PostBox = React.createClass({
 
         return (
             <div className={ wrapperDivClasses }>
-                <div className="row">
-                    <Link to={`/${ this.props.programSlug }/${ this.props.id }`}><img className="post-box-image col-md-12"/></Link>
+                <div className="row post-box-image-container">
+                    <Link to={`/${ this.props.programSlug }/${ this.props.id }`}><img src="img/example.jpg" className="post-box-image col-md-12"/></Link>
                 </div>
             	<h2><Link to={`/${ this.props.programSlug }/${ this.props.id }`}>{ this.props.title }</Link></h2>
                 <p>{ this.props.body }</p>
--- frontend/app/components/SidebarPostList.js
+++ frontend/app/components/SidebarPostList.js
@@ -9,13 +9,13 @@ var SidebarPostList = React.createClass({
             	<h3>{ this.props.title }</h3>
             	<ul className="sidebar-post-list">
             		<li className="sidebar-post-list-element row"><div className="col-xs-12">
-                        <img className="post-image pull-left" src="test.png" />
+                        <img className="post-image pull-left" src="img/example2.jpg" />
                         <div className="post-program">GARASJEN</div>
                         <div className="post-title">Trumpfer dronene F35?</div>
                         <div className="post-lead">Vi snakker om Chromecast, er det egentlig bra?</div>
                     </div></li>
             		<li className="sidebar-post-list-element row"><div className="col-xs-12">
-                        <img className="post-image pull-left" src="test.png" />
+                        <img className="post-image pull-left" src="img/example2.jpg" />
                         <div className="post-program">GARASJEN</div>
                         <div className="post-title">Trumpfer dronene F35?</div>
                         <div className="post-lead">Vi snakker om Chromecast, er det egentlig bra?</div>
